2de4dab71bbb40416de00e551cb1f0fdb7e0bfbb
Natan Rocha <natanrochabezerra@gmail.com>
2022-11-28 16:10:53 -0300

    atualização

--- news-homepage-main/index.js
+++ news-homepage-main/index.js
@@ -1,11 +1,53 @@
-const navbar = document.querySelector("nav");
-const openMenu = document.getElementById("menu-button");
-const closeMenu = document.getElementById("menu-close");
+import "./style.css";
 
-openMenu.addEventListener("click", () => {
-  navbar.classList.add("open");
-});
+const toggleOpenAttribute = (arrayOfItems) => {
+	arrayOfItems.forEach((i) => {
+		const isOpen = i.hasAttribute("open");
+		isOpen ? i.removeAttribute("open") : i.setAttribute("open", "");
+	});
+};
 
-closeMenu.addEventListener("click", () => {
-  navbar.classList.remove("open");
+const toggleTabIndexes = (arrayOfItems) => {
+	arrayOfItems.forEach((i) => {
+		const isTabbable = i.tabIndex > -1;
+		isTabbable
+			? i.setAttribute("tabindex", "-1")
+			: i.setAttribute("tabindex", "0");
+	});
+};
+
+// nav
+const mobileNavBtn = document.querySelector("#mobileNavBtn");
+const mobileNav = document.querySelector("#mobileNav");
+const navLinks = document.querySelectorAll(".nav-link");
+const closeBtn = document.querySelector("#closeBtn");
+const navBkg = document.querySelector(".nav-bkg");
+
+const handleMobileNavClick = (e) => {
+	const isOpen = JSON.parse(e.target.getAttribute("aria-expanded"));
+	e.target.setAttribute("aria-expanded", !isOpen);
+	toggleOpenAttribute([mobileNav, navBkg]);
+	toggleTabIndexes([...navLinks, closeBtn]);
+};
+
+const closeMobileNav = () => {
+	mobileNavBtn.setAttribute("aria-expanded", "false");
+	toggleOpenAttribute([mobileNav, navBkg]);
+	toggleTabIndexes([...navLinks, closeBtn]);
+};
+
+// mutation observer to close down mobile nav
+
+// event listeners
+mobileNavBtn.addEventListener("click", handleMobileNavClick);
+
+navBkg.addEventListener("click", closeMobileNav);
+
+closeBtn.addEventListener("click", closeMobileNav);
+
+window.addEventListener("keydown", (e) => {
+	if (!navBkg.hasAttribute("open")) {
+		return;
+	}
+	e.key === "Escape" && closeMobileNav();
 });
